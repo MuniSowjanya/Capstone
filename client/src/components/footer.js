7228659d7aff8cc6e8b17729ae2e0bad7fa3e1fb
munisowjanya <munisowjanya.com>
2020-10-08 17:37:26 +0530

    client

---
+++ client/src/components/footer.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import logo from './logo.jpg';
+import '../static/css/app.css';
+class Footer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+    render() {
+        return (
+            <div id="footer" className="container-fluid">
+  <hr/>
+                <div id="footer-detail">
+                  
+                    <img src={logo} alt="Logo" id="flogo" height="60" width="60" />
+                    <small>&copy; Copyright 2020, Soal</small>
+                    <div style={{ float: "right" ,color:"white"}}>
+                        <a href="">Location Map</a> <br></br>
+                        <a href="">Popular</a> <br></br>
+                        <a href="">Term and use</a> <br></br>
+                        <a href="">Help & Contact Us</a>
+                        
+                    </div>
+
+                </div>
+
+
+            </div>
+
+
+        );
+    }
+}
+
+export default Footer;
